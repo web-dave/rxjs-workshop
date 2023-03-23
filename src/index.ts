c001ac0ff75a9c3e41526c9ce819798b431453d1
web-dave <dave@webdave.de>
2023-03-23 09:40:26 +0100

    foo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {} from 'rxjs';
+import { fromEvent, Subscription } from 'rxjs';
 
 const btn = document.querySelector('button');
 const output: HTMLUListElement = document.querySelector('ul');
@@ -10,3 +10,10 @@ function print(text: string) {
 }
 
 // coding start here
+const event$ = fromEvent(btn, 'click');
+
+const sub: Subscription = event$.subscribe((data) => print('Hellö Wöald!'));
+
+setTimeout(() => {
+  sub.unsubscribe();
+}, 5000);
