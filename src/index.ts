f10fa28f3a4de25cadb1b255a577808c97b9836a
Dave <dave@webdave.de>
2023-10-16 16:59:45 +0200

    map flattening

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 import {
   Observable,
   Observer,
+  concatMap,
+  debounceTime,
+  exhaustMap,
   fromEvent,
   map,
+  mergeMap,
   pairwise,
+  switchMap,
   take,
   tap,
   timer,
 } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
 
 const btn = document.querySelector('button');
+const input = document.querySelector('input');
 const output: HTMLUListElement = document.querySelector('ul');
 
 function print(text: string) {
@@ -20,7 +27,50 @@ function print(text: string) {
 
 // coding start here
 
+interface User {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  gender: string;
+  ip_address: string;
+  eye_color: string;
+}
+
+const inp$ = fromEvent(input, 'input').pipe(
+  map((data) => (data.target as HTMLInputElement).value)
+);
+
+inp$
+  .pipe(
+    // debounceTime(500),
+    exhaustMap((search) =>
+      ajax<User[]>(
+        'http://localhost:3000/users?first_name_like=' + search
+      ).pipe(map((data) => data.response))
+    ),
+    map((data: User[]) => data.map((u) => u.first_name + ',' + u.last_name))
+  )
+  .subscribe({
+    next: (data) => {
+      data.forEach((u) => print(u));
+    },
+  });
+
 const obs$ = fromEvent(btn, 'click');
+
+obs$
+  .pipe(
+    map((data) => data.timeStamp),
+    take(7),
+    pairwise(),
+    map((data) => data[1] - data[0]),
+    // map(([acc, curr]) => curr - acc),
+    map((data) => data + '')
+  )
+  .subscribe({
+    next: (data) => print(data),
+  });
 
 // const obs = {
 //   observer: null,
@@ -39,19 +89,6 @@ const obs$ = fromEvent(btn, 'click');
 //     tap((data) => console.log(data))
 //   )
 //   .subscribe();
-
-obs$
-  .pipe(
-    map((data) => data.timeStamp),
-    take(7),
-    pairwise(),
-    map((data) => data[1] - data[0]),
-    // map(([acc, curr]) => curr - acc),
-    map((data) => data + '')
-  )
-  .subscribe({
-    next: (data) => print(data),
-  });
 
 // obs.next('Hallo');
 
