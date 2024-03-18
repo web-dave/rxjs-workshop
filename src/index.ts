39bdcfd0922236b02e1b51a2d02cc202dd4e9ca7
Dave <dave@webdave.de>
2024-03-18 09:41:13 +0100

    Mein erstes Observable

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import {} from 'rxjs';
-
 const btn = document.querySelector('button');
 const output: HTMLUListElement = document.querySelector('ul');
 
@@ -10,3 +8,15 @@ function print(text: string) {
 }
 
 // coding start here
+const myObservable = {
+  observers: [],
+  subscribe: function (observer) {
+    myObservable.observers.push(observer);
+    myObservable.next('Hallo');
+  },
+  next: function (data: any) {
+    myObservable.observers.forEach((o) => o.next(data));
+  },
+};
+
+myObservable.subscribe({ next: (foo) => console.log(foo) });
