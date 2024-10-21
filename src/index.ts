e4752d364516fa18742d3800de3c1d0108878af7
Dave <dave@webdave.de>
2024-10-21 14:35:52 +0200

    merge and flattening

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 import {
+  concatMap,
+  debounceTime,
+  exhaustMap,
   fromEvent,
   map,
+  mergeMap,
   Observable,
   pairwise,
   PartialObserver,
+  switchMap,
   take,
   tap,
 } from 'rxjs';
 
+import { ajax } from 'rxjs/ajax';
+
 const btn = document.querySelector('button');
+const input = document.querySelector('input');
 const output: HTMLUListElement = document.querySelector('ul');
 
 function print(text: string) {
@@ -32,3 +40,22 @@ const myObservable: Observable<string> = fromEvent(btn, 'click').pipe(
 myObservable.subscribe({
   next: (data) => print(data),
 });
+
+const input$: Observable<string> = fromEvent(input, 'input').pipe(
+  debounceTime(500),
+  map((e) => (e.target as HTMLInputElement).value)
+);
+
+const http$ = input$
+  .pipe(
+    switchMap((search) =>
+      ajax('http://localhost:3000/users?first_name_like=' + search).pipe(
+        map((data) => data.response)
+      )
+    ),
+    tap((data) => console.log(data)),
+    map((userList: any[]) =>
+      userList.map((user) => user.first_name + ', ' + user.last_name)
+    )
+  )
+  .subscribe((data) => data.forEach((name) => print(name)));
